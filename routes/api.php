074d07ce79847ed4ebb91ff01f662d084b05b206
avhkbakhshaliyev <kamranbaxshaliyev567@gmail.com>
2025-04-07 23:42:00 +0400

    user route

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,6 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('user', [AuthController::class, 'user']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
